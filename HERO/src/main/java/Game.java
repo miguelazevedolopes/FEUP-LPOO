87a25980362443d113f84b1bcec67814b36919b1
Miguel Lopes <lopesazevedom@gmail.com>
2021-02-26 15:49:27 +0000

    finished ex 6

---
+++ HERO/src/main/java/Game.java
@@ -0,0 +1,36 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private Screen screen;
+
+    public Game(){
+        Terminal terminal = null;
+        try {
+            terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);   // we don't need a cursor
+            screen.startScreen();             // screens must be started
+            screen.doResizeIfNecessary();     // resize screen if necessary
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void draw(){
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        try {
+            screen.refresh();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void run(){
+        draw();
+    }
+}
