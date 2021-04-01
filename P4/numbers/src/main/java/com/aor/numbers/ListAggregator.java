38cc787c3ae4db78d779340162247f903aeee114
Miguel Lopes <lopesazevedom@gmail.com>
2021-04-01 18:51:57 +0100

    working on P4

--- P4/numbers/src/main/java/com/aor/numbers/ListAggregator.java
+++ P4/numbers/src/main/java/com/aor/numbers/ListAggregator.java
@@ -25,7 +25,9 @@ public class ListAggregator {
      * @return The maximum value in the list.
      */
     public Integer max(List<Integer> list) {
-        int max = 0;
+        if (list.size()<1)
+            return 0;
+        int max =list.get(0);
 
         for (Integer number : list)
             if (number > max)
@@ -52,8 +54,7 @@ public class ListAggregator {
      * Counts the number of distinct numbers in a list.
      * @return The number of distinct numbers.
      */
-    public int distinct(List<Integer> list) {
-        ListDeduplicator deduplicator = new ListDeduplicator();
+    public int distinct(List<Integer> list,GenericListDeduplicator deduplicator) {
         List<Integer> distinct = deduplicator.deduplicate(list);
 
         return distinct.size();
--- P4/numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ P4/numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -1,16 +1,22 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListAggregatorTest {
+    private List<Integer> list;
+
+    @BeforeEach
+    public void helper(){
+        list=Arrays.asList(1,2,4,2,5);
+    }
+
     @Test
     public void sum() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
 
@@ -19,8 +25,6 @@ public class ListAggregatorTest {
 
     @Test
     public void max() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
 
@@ -28,9 +32,15 @@ public class ListAggregatorTest {
     }
 
     @Test
-    public void min() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
+    public void max_bug_7263(){
+        List<Integer> givenList=Arrays.asList(-1,-4,-5);
+        ListAggregator aggregator =new ListAggregator();
+        int max=aggregator.max(givenList);
+        Assertions.assertEquals(-1,max);
+    }
 
+    @Test
+    public void min() {
         ListAggregator aggregator = new ListAggregator();
         int min = aggregator.min(list);
 
@@ -39,11 +49,19 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
-        int distinct = aggregator.distinct(list);
+        ListDeduplicator deduplicator = new ListDeduplicator();
+        int distinct = aggregator.distinct(list,deduplicator);
 
         Assertions.assertEquals(4, distinct);
+    }
+
+    @Test
+    public void distinct_bug_8726(){
+        List<Integer> givenList=Arrays.asList(1,2,4,2);
+        ListAggregator aggregator =new ListAggregator();
+        ListDeduplicator deduplicator = new ListDeduplicator();
+        int number=aggregator.distinct(givenList,deduplicator);
+        Assertions.assertEquals(3,number);
     }
 }
